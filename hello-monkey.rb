85f9c6b3e64501cb061b935d70d5e6d8e5ac9a33
Matthew Du Pont <github@matthewfdupont.com>
2017-01-19 08:04:24 -0800

    recognize number, play mp3

--- hello-monkey.rb
+++ hello-monkey.rb
@@ -3,7 +3,12 @@ require 'sinatra'
 require 'twilio-ruby'
 
 get '/hello-monkey' do
+	people = {
+		'+14154848153' => 'Dennis'
+	}
+	name = people[params['From']] || "you goof"
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello Monkey'
+    r.Say 'Hello #{name}'
+    r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
   end.text
 end
