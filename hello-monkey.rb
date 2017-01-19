4f433ab71ba9c93f505828166e7d8f6da48bad3a
Matthew Du Pont <github@matthewfdupont.com>
2017-01-19 08:21:14 -0800

    redirect and gather tutorial

--- hello-monkey.rb
+++ hello-monkey.rb
@@ -8,7 +8,19 @@ get '/hello-monkey' do
 	}
 	name = people[params['From']] || "you goof"
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello #{name}'
+    r.Say "Hello #{name}"
     r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
+    r.Gather numDigits: '1', action: '/hello-monkey/handle-gather', method: 'get' do |g|
+    	g.Say 'To speak to a real monkey, press 1.'
+    	g.Say 'Press any other key to start over.'
+    end
   end.text
+end
+
+get '/hello-monkey/handle-gather' do
+	redirect '/hello-monkey' unless params['Digits'] == '1'
+	Twilio::TwiML::Response.new do |r|
+		r.Dial '+13105551212' ## Connect to demo of Koko
+		r.Say 'The call failed or the remote party hung up. Goodbye!'
+	end.text
 end
